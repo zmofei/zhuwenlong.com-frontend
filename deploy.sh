6ee032d9eabb3d7480b1ae427b240fc9797e0aaf
zmofei <13761509829@163.com>
2019-05-08 19:12:15 +0800

    add travis

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+FileName=$(date +%y_%m_%d_%H_%M_%S)
+rm -rf ./build
+npm run build
+tar -cvzf build_$FileName.tar.gz ./build
+scp build_$FileName.tar.gz root@47.98.142.75:/usr/local/zhuwenlong/www/zhuwenlong.com-frontend
+rm -rf build_$FileName.tar.gz
+ssh root@47.98.142.75 "
+cd /usr/local/zhuwenlong/www/zhuwenlong.com-frontend
+cp -r build build_bck_$FileName
+tar -xvzf build_$FileName.tar.gz
+"
