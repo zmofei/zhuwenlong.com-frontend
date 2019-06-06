3c1ed815df27eca86ade0dd126758702aa52ba60
zmofei <13761509829@163.com>
2019-06-06 18:09:04 +0800

    update deploy address

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@ FileName=$(date +%y_%m_%d_%H_%M_%S)
 rm -rf ./build
 PUBLIC_URL="//cdn.zhuwenlong.com" npm run build
 tar -cvzf build_$FileName.tar.gz ./build
-scp build_$FileName.tar.gz root@47.98.142.75:/usr/local/zhuwenlong/www/zhuwenlong.com-frontend
+scp build_$FileName.tar.gz root@47.110.229.91:/usr/local/zhuwenlong/www/zhuwenlong.com-frontend
 rm -rf build_$FileName.tar.gz
-ssh root@47.98.142.75 "
+ssh root@47.110.229.91 "
 cd /usr/local/zhuwenlong/www/zhuwenlong.com-frontend
 cp -r build build_bck_$FileName
 tar -xvzf build_$FileName.tar.gz
