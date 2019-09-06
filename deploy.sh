8c573727655eaf289478e974ac17dc87b209c3e1
zmofei <13761509829@163.com>
2019-09-06 19:13:18 +0800

    debug action

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,12 @@ npm install
 FileName=$(date +%y_%m_%d_%H_%M_%S)
 rm -rf ./build
 PUBLIC_URL="//cdn.zhuwenlong.com" npm run build
+echo "packup the tar"
 tar -cvzf build_$FileName.tar.gz ./build
+echo "scp file"
 scp build_$FileName.tar.gz root@47.110.229.91:/usr/local/zhuwenlong/www/zhuwenlong.com-frontend
 rm -rf build_$FileName.tar.gz
+echo "login to deploy"
 ssh root@47.110.229.91 "
 cd /usr/local/zhuwenlong/www/zhuwenlong.com-frontend
 cp -r build build_bck_$FileName
