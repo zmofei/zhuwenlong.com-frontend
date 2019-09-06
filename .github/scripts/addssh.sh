bd0c3bfa1ec70cf0721af0d4814b145f08077180
zmofei <13761509829@163.com>
2019-09-06 19:01:56 +0800

    add rss

--- .github/scripts/addssh.sh
+++ .github/scripts/addssh.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-mkdir $HOME/secrets
+SSH_PATH="$HOME/.ssh"
+mkdir -p "$SSH_PATH"
+touch "$SSH_PATH/known_hosts"
+
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SSH_S" \
-  --output $HOME/secrets/deploy_rsa deploy_rsa.gpg
-eval "$(ssh-agent -s)"
-chmod 600 $HOME/secrets/deploy_rsa
-ssh-add $HOME/secrets/deploy_rsa
+  --output $SSH_PATH/deploy_rsa deploy_rsa.gpg
 
-ssh-keyscan -H 47.110.229.91 >> ~/.ssh/known_hosts
+chmod 700 "$SSH_PATH"
+chmod 600 "$SSH_PATH/known_hosts"
+chmod 600 "$SSH_PATH/deploy_rsa"
+
+eval "$(ssh-agent -s)"
+ssh-add $SSH_PATH/deploy_rsa
+ssh-keyscan -t rsa 47.110.229.91 >> "$SSH_PATH/known_hosts"
