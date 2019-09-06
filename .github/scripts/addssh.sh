dc781f953f71686862f91e22d22c3e40954f647e
zmofei <13761509829@163.com>
2019-09-06 18:16:35 +0800

    change secrets dir

--- .github/scripts/addssh.sh
+++ .github/scripts/addssh.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir /tmp
+mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SSH_S" \
-  --output /tmp/deploy_rsa deploy_rsa.gpg
+  --output $HOME/secrets/deploy_rsa deploy_rsa.gpg
 eval "$(ssh-agent -s)"
-chmod 600 /tmp/deploy_rsa
-ssh-add /tmp/deploy_rsa
+chmod 600 $HOME/secrets/deploy_rsa
+ssh-add $HOME/secrets/deploy_rsa
