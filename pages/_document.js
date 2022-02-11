a1fb3e2d6068357ddf692828d6006b7bc4d03073
Mofei Zhu <13761509829@163.com>
2022-02-11 19:09:56 +0800

    update

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=G-TDFKD4X8PR`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'TDFKD4X8PR', {
+                                    page_path: window.location.pathname,
+                                });
+                            `,
+                        }}
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
