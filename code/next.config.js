988d3df0ab454d67ffc18379b730466e8783971c
Mofei Zhu <13761509829@163.com>
2022-02-14 19:27:12 +0800

    updat

--- code/next.config.js
+++ code/next.config.js
@@ -2,7 +2,11 @@ const withImages = require("next-images");
 
 module.exports = {
     ...withImages(),
+    swcMinify: true,
     images: {
         disableStaticImages: true,
+    },
+    experimental: {
+        outputStandalone: true,
     },
 }
