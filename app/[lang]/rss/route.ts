25ef2a8c650f752bf9e2ef45cbaf0d7689a43a5e
Mofei Zhu <13761509829@163.com>
2025-01-07 22:09:26 +0200

    Add RSS (#41)
    * update
    
    * update

---
+++ app/[lang]/rss/route.ts
@@ -0,0 +1,47 @@
+var RSS = require('rss');
+
+export async function GET(request: Request,
+    { params }: { params: Promise<{ lang: string }> }) {
+
+    const lang = (await params).lang
+
+    const URL = `https://www.mofei.life/api/blog/list/1?lang=${lang}&pagesize=20&html=1`;
+    const blogsRst = await fetch(URL)
+    const blogs = await blogsRst.json()
+
+
+    const feedOptions = {
+        title: lang === 'zh' ? '你好我是Mofei' : 'Hi! I am Mofei!',
+        description: `Mofei Zhu, a software engineer from China, sharing life and work experiences in Finland, exploring tech, family, and cultural adventures. ${lang == 'zh' ? "feedId:99544572437916672+userId:73749889001453568" : "feedId:99543735457776640+userId:73749889001453568"}  `,
+        feed_url: `https://www.mofei.life/${lang}/rss`,
+        site_url: `https://www.mofei.life/${lang}/`,
+        image_url: 'https://www.mofei.life/img/mofei-logo_500_500.svg',
+        docs: 'https://cyber.harvard.edu/rss/rss.html',
+        managingEditor: `hi@mofei.life (${lang === 'zh' ? '朱文龙' : 'Mofei Zhu'})`,
+        webMaster: `hi@mofei.life (${lang === 'zh' ? '朱文龙' : 'Mofei Zhu'})`,
+        language: lang,
+        lastBuildDate: new Date().toUTCString(),
+    }
+
+    const feed = new RSS(feedOptions);
+
+    blogs.data.forEach((blog: any) => {
+        feed.item({
+            title: blog.title,
+            description: `<![CDATA[${blog.html}]]>`,
+            url: `https://www.mofei.life/${lang}/blog/article/${blog._id}`,
+            guid: blog.id,
+            author: lang === 'zh' ? '朱文龙' : 'Mofei Zhu',
+            date: new Date(blog.pubtime).toUTCString()
+        })
+    });
+
+    // Response.headers.append("content-type", "text/xml");
+
+    return new Response(feed.xml({ indent: true }),
+        {
+            headers: {
+                'Content-Type': 'text/xml',
+            },
+        })
+}
---
+++ app/[lang]/rss_comments/route.ts
@@ -0,0 +1,47 @@
+var RSS = require('rss');
+
+export async function GET(request: Request,
+    { params }: { params: Promise<{ lang: string }> }) {
+
+    const lang = (await params).lang
+
+    const URL = `https://www.mofei.life/api/blog/comment/list/0/1?itemsPerPage=20`;
+    const blogsRst = await fetch(URL)
+    const blogs = await blogsRst.json()
+
+
+    const feedOptions = {
+        title: lang === 'zh' ? '你好我是Mofei' : 'Hi! I am Mofei!',
+        description: "Mofei Zhu, a software engineer from China, sharing life and work experiences in Finland, exploring tech, family, and cultural adventures.",
+        feed_url: `https://www.mofei.life/${lang}/rss`,
+        site_url: `https://www.mofei.life/${lang}/`,
+        image_url: 'https://www.mofei.life/img/mofei-logo_500_500.svg',
+        docs: 'https://cyber.harvard.edu/rss/rss.html',
+        managingEditor: `hi@mofei.life (${lang === 'zh' ? '朱文龙' : 'Mofei Zhu'})`,
+        webMaster: `hi@mofei.life (${lang === 'zh' ? '朱文龙' : 'Mofei Zhu'})`,
+        language: lang,
+        lastBuildDate: new Date().toUTCString(),
+    }
+
+    const feed = new RSS(feedOptions);
+
+    blogs.data.forEach((blog: any) => {
+        console.log(blog)
+        feed.item({
+            title: `${blog.name}'s comment`,
+            description: `${blog.content}`,
+            url: blog.blogId === '000000000000000000000000' ? `https://www.mofei.life/${lang}/message` : `https://www.mofei.life/${lang}/blog/article/${blog.blogId}`,
+            guid: blog.id,
+            author: `${blog.name} (${blog.blog})`,
+            date: new Date(blog.time).toUTCString()
+        })
+    });
+
+
+    return new Response(feed.xml({ indent: true }),
+        {
+            headers: {
+                'Content-Type': 'text/xml',
+            },
+        })
+}
